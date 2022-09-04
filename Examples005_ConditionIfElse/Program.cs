a5a4764c96305ebe4d065de99020701321a83b78
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-04 19:42:34 +0300

    Examples 005 if else condition added

---
+++ Examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша") {
+    Console.WriteLine("Ура, это же Маша!");
+} else {
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
