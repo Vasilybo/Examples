ebe933322ebebd92969ba7af9fe5509dea438179
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-05 14:52:31 +0300

    Refactor Example 007 for true triangle

--- Examples007_Magic/Program.cs
+++ Examples007_Magic/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Clear();
-// Console.SetCursorPosition(10, 4);
-// Console.WriteLine("+");
+Console.SetCursorPosition(10, 4);
+Console.Write("+");
 
 int xa = 40, ya = 1,
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.Write("+");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+Console.Write("+");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.Write("+");
 
 int x = xa, y = xb;
 
@@ -38,6 +38,6 @@ while (count < 1000)
         y = (y + yc) / 2;
 }
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
+    Console.Write("+");
     count++;
 }
